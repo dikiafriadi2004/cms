beac095dc58bd2254ab1e1d09a50e6c4acdab9ad
Diki Afriadi <diki.afriadi2004@gmail.cim>
2025-02-20 11:47:23 +0700

    update

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -3,20 +3,29 @@
         <a class="navbar-brand" href="#">
             <img src="{{ asset('frontend/assets/images/logo.png') }}" class="logo" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                @foreach($menus as $value)
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is($value->slug) ? 'active' : '' }}" href="{{ url($value->slug) }}">{{ $value->menu }}</a>
-                    </li>
+                @foreach ($menus as $value)
+                    @if (Route::currentRouteName() == 'blog.index')
+                        <li class="nav-item">
+                            <a href="{{ url($value->slug) }}" class="nav-link active">
+                                {{ $value->menu }}
+                            </a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is($value->slug) || request()->is($value->slug . '/*') ? 'active' : '' }}"
+                                href="{{ url($value->slug) }}">
+                                {{ $value->menu }}
+                            </a>
+                        </li>
+                    @endif
                 @endforeach
-
             </ul>
         </div>
     </div>
