f1094f1a2b095c55ce5f5c0ac553d553deb31e5f
Diki Afriadi <diki.afriadi2004@gmail.cim>
2025-02-24 10:06:32 +0700

    fix bug menu

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -9,22 +9,13 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
                 @foreach ($menus as $value)
-                    @if (Route::currentRouteName() == 'blog.index')
-                        <li class="nav-item">
-                            <a href="{{ url($value->slug) }}" class="nav-link active">
-                                {{ $value->menu }}
-                            </a>
-                        </li>
-                    @else
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is($value->slug) || request()->is($value->slug . '/*') ? 'active' : '' }}"
-                                href="{{ url($value->slug) }}">
-                                {{ $value->menu }}
-                            </a>
-                        </li>
-                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->is(Str::replaceFirst('/', '', $value->slug)) || request()->is(Str::replaceFirst('/', '', $value->slug) . '/*') ? 'active' : '' }}"
+                            href="{{ url($value->slug) }}">
+                            {{ $value->menu }}
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         </div>
